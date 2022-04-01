ad5c2abf5e5fe7f50eb7b6e4b605a3518757a829
akmaljon <akmaljon.ahmedjanov@yandex.ru>
2022-04-01 10:11:15 +0500

    add js

---
+++ js/main.js
@@ -0,0 +1,75 @@
+let x = 5;
+let a = 4;
+let b = 8;
+let sh = 9;
+let ch = 3;
+let yu = 10;
+let gh = "salom";
+let lk = "hello";
+let hp = "55";
+let re = 33;
+
+ch = 8;
+console.log(a);
+
+const d = "salom";
+const dev = "salom";
+const ff = "salom";
+const jk= "salom";
+const qe = "salom";
+const fg = "salom";
+const di = "salom";
+const du = "salom";
+const desc = "salom";
+const good = "salom";
+
+console.log(good);
+console.log(x);
+const akmal = 30;
+console.log(akmal);
+
+
+// DATA TYPES
+// MALUMOT TURLARI
+
+let youName = "Bilol"; //string
+
+console.log(youName);
+
+let youAge = 30; //number//integer
+let youwhole = 70.02;//float
+
+console.log(youwhole);
+console.log(youAge);
+
+let youId = 4998694909689400496n; //big integer
+
+console.log(youId);
+
+let myAge = 34;//boolean
+
+console.log(34==5);//false
+console.log(34 == 34);//true
+
+let you; //undefined
+
+console.log(you);//undefined //Aniqlanmagan qiymat
+
+let hi = null; //null
+
+console.log(hi); //null //noll qiymat
+
+let hiName = Symbol ("Axmad");//Symbol
+
+console.log(hiName);
+
+let boxer = {
+  name: 'Ali',
+  Age: 20,
+  whole: 57,
+  category: "wbo",
+  defeat: 5,
+  winning: 10
+};
+
+console.log(boxer);//object type
